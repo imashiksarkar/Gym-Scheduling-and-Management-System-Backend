4e75b8f0685c85bb271d41fb1e1d625a88bda296
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2025-05-30 22:48:04 +0600

    - fix. test - fix. remove .only from auth module

--- src/modules/auth/__test__/auth.spec.ts
+++ src/modules/auth/__test__/auth.spec.ts
@@ -10,7 +10,7 @@ import {
 } from '../../../test/utils'
 import authService from '../auth.service'
 
-describe.only('Auth Module', async () => {
+describe('Auth Module', async () => {
   const app = await getAppInstance()
 
   describe('Role: Trainee', () => {
